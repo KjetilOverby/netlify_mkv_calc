83d52e5723bc0176ab9359feb713da22c1c622d9
Kjetil Øverby <kjetiloverby@imac.home>
2020-12-27 10:14:55 +0100

    api

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
-export default function Index() {
+export default function Index({ stars }) {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
@@ -15,6 +15,7 @@ export default function Index() {
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
+          {stars.id}
         </Link>
         <ProTip />
         <Copyright />
@@ -22,3 +23,8 @@ export default function Index() {
     </Container>
   );
 }
+Index.getInitialProps = async (ctx) => {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js');
+  const stars = await res.json();
+  return { stars };
+};
